43ef817033627e722e70adf55261efacbbca7bf7
Refat Shikder <43178208+Refat-Shikder@users.noreply.github.com>
2021-06-24 20:26:15 +0600

    First-Commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,123 @@
+// let js = `amazing`;
+// let person = `aladin`;
+// let PI = 3.1416;
+
+// let myFirstJob = `pro grammar`;
+// let myLastJob = `tea chair`;
+
+// console.log(myFirstJob);
+// console.log(myLastJob);
+// console.log(2303-323>2303-233)
+
+//  let marksMass = 95;
+//  let johnsMass = 85;
+//  let marksHig = 1.88;
+//  let johnsHig = 1.76;
+
+//  let johnBmi = johnsMass / johnsHig ** 2;
+//  let markBmi = marksMass / marksHig ** 2;
+
+// console.log(markBmi,johnBmi,markBmi>johnBmi)
+
+// const firstName = `aladin`;
+// const job = `student`;
+// const birthYear = 2000;
+// const year = 2021;
+
+// const aladin = `I'm ` + firstName + ` , a ` + (year - birthYear)+ ` old `+job+`!`
+// console.log(aladin)
+
+// const aladinNew = `I'm ${firstName} , a ${year - birthYear} old ${job}! `
+// console.log(aladinNew)
+
+// console.log(`this is a regular string
+// and this is a abnormal
+// string`)
+// const age = 17;
+
+// if (age >= 18) {
+//   console.log(`you can apply for driving license 🚗`);
+// }else{
+//     const leftYear =18 - age
+//     console.log(`yoou are too young madafaka.you have to wait for ${leftYear} more years`)
+// }
+
+
+// const birthYear = 2002 
+// let century 
+
+// if (birthYear <=2000) {
+//     century = 20
+// }else{
+//     century = 21;
+
+// }
+// console.log(century)
+
+//  let marksMass = 78;
+//  let johnsMass = 92;
+//  let marksHig = 1.69;
+//  let johnsHig = 1.95;
+
+//  let johnBmi = johnsMass / johnsHig ** 2;
+//  let markBmi = marksMass / marksHig ** 2;
+
+//  console.log(markBmi,johnBmi,markBmi>johnBmi)
+
+// if (markBmi>johnBmi) {
+//     console.log(`marks bmi (${markBmi}) is higher then johns bmi (${johnBmi})`);
+// }else{
+//         console.log(`johns bmi (${johnBmi}) is higher then marks bmi (${markBmi})`);
+// }
+// const inYear=  `1991`
+// console.log(Number(inYear),inYear)
+// console.log(`aladin `)
+
+
+// console.log(Boolean(NaN))
+
+
+// const fav = Number(prompt(`enter a number`))
+// console.log(fav);
+// console.log(typeof fav);
+// if (fav<=50) {
+//     console.log(`${fav} is a good number`)
+// }
+// let dolphinsAvr =( 96 + 108+ 89)/3
+// let koalasAvr =(88 + 91+ 110)/3
+
+
+// //@ts-check
+
+//      if (dolphinsAvr > koalasAvr && dolphinsAvr >=100) {
+//        console.log(`Dolphins won the match`);
+//      }else if(koalasAvr >dolphinsAvr&&koalasAvr>=100){
+//  console.log(`koalas won the match`);
+//      } else if (dolphinsAvr === koalasAvr&& dolphinsAvr>=100 && koalasAvr>=100) {
+//        console.log(`the match has been draw`);
+//      } else {
+//       console.log(`none won the match`)
+//      }
+
+
+// let bill = 40
+// let tip15 = (bill / 100)* 15
+// let tip20 = (bill / 100) * 20;
+
+// let payBill =
+//   bill >= 50 && bill <= 300
+//     ? console.log(
+//         `please pay total $${tip15 + bill} (bill is ${bill} & tip is ${tip15})`
+//       )
+//     : console.log(
+//         `please pay total $${tip20 + bill} (bill is ${bill} & tip is ${tip20})`
+//       );
+
+
+      const bill = 40
+    const tip = bill <=300 && bill >= 50 ? bill * .15 : bill * 0.2
+    console.log(
+      `please pay total $${tip + bill} (bill is ${bill} & tip is ${tip})`
+    );
+
+    // end of
